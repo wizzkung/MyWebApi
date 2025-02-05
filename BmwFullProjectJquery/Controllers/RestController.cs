cb534c485db869c7cacde10b77035ac6089e3310
Arthur Pak <sandtorus@gmail.com>
2025-02-05 21:34:54 +0500

    ***

--- BmwFullProjectJquery/Controllers/RestController.cs
+++ BmwFullProjectJquery/Controllers/RestController.cs
@@ -5,6 +5,8 @@ using Dapper;
 using System.Data;
 using BmwFullProjectJquery.Model;
 using BmwFullProjectJquery.DTO;
+using OfficeOpenXml;
+using System.Text;
 
 namespace BmwFullProjectJquery.Controllers
 {
@@ -84,7 +86,7 @@ namespace BmwFullProjectJquery.Controllers
             {
                 await db.OpenAsync();
                 var res = await db.ExecuteAsync("pBmw;4", new { @name = bmw.name, @seriesName = bmw.seriesName }, commandType: System.Data.CommandType.StoredProcedure);
-                return Ok("Новая модель успешно добавлена");
+                return Ok($"Новая модель успешно добавлена - {res}");
             }
             catch (Exception ex)
             {
@@ -100,13 +102,116 @@ namespace BmwFullProjectJquery.Controllers
             {
                 await db.OpenAsync();
                 var res = await db.ExecuteAsync("pBmw;6", new { @oldName = bmw.oldName, @newName = bmw.dto.name, @seriesName = bmw.dto.seriesName }, commandType: System.Data.CommandType.StoredProcedure);
-                return Ok("Модель успешно обновлена");
+                return Ok($"Модель успешно обновлена - {res}");
             }
             catch (Exception ex)
             {
                 return BadRequest(ex.Message);
             }
         }
+
+        [HttpDelete, Route("Delete/{modelName}/{seriesName}")]
+        public async Task<IActionResult> DeleteModel([FromRoute] string modelName, [FromRoute] string seriesName)
+        {
+            await using var db = new SqlConnection(_configuration["db"]);
+            try
+            {
+                await db.OpenAsync();
+                var res = await db.ExecuteAsync("pBmw;7", new { @modelName = modelName, @seriesName = seriesName }, commandType: System.Data.CommandType.StoredProcedure);
+                return Ok($"Модель успешно  удалена - {res}");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet, Route("excel")]
+        public async Task<IActionResult> LoadExcel()
+        {
+            await using var db = new SqlConnection(_configuration["db"]);
+            try
+            {
+                await db.OpenAsync();
+                var res = db.Query("pBmw", commandType: System.Data.CommandType.StoredProcedure);
+                if (res == null || !res.Any())
+                    return BadRequest();
+
+                using var package = new ExcelPackage();
+                var worksheet = package.Workbook.Worksheets.Add("Data");
+
+                var columns = ((IDictionary<string, object>)res.First()).Keys.ToList();
+                for (int i = 0; i < columns.Count; i++)
+                {
+                    worksheet.Cells[1, i + 1].Value = columns[i];
+                }
+
+                int row = 2;
+                foreach (dynamic item in res)
+                {
+                    var values = ((IDictionary<string, object>)item).Values;
+                    for (int col = 0; col < values.Count; col++)
+                    {
+                        worksheet.Cells[row, col + 1].Value = values.ElementAt(col);
+                    }
+                    row++;
+                }
+
+                worksheet.Cells[worksheet.Dimension.Address].AutoFitColumns();
+
+                // Возвращаем файл
+                var stream = new MemoryStream(package.GetAsByteArray());
+                return File(stream, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "Report.xlsx");
+
+            }
+
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+
+        [HttpGet, Route("csv")]
+        public async Task<IActionResult> LoadCsv()
+        {
+            await using var db = new SqlConnection(_configuration["db"]);
+            try
+            {
+                await db.OpenAsync();
+                var res = db.Query("pBmw", commandType: CommandType.StoredProcedure);
+
+                if (res == null || !res.Any())
+                    return BadRequest("Нет данных для выгрузки");
+
+                var firstRow = (IDictionary<string, object>)res.First();
+                var columns = firstRow.Keys.ToList();
+
+                
+                var sb = new StringBuilder();
+
+                // Заголовок
+                sb.AppendLine(string.Join(";", columns));
+
+                // Данные
+                foreach (dynamic row in res)
+                {
+                    var dict = (IDictionary<string, object>)row;
+                    var line = string.Join(";", dict.Values.Select(v => v?.ToString().Replace(";", ",")));
+                    sb.AppendLine(line);
+                }
+
+                var bytes = Encoding.UTF8.GetBytes(sb.ToString());
+                return File(new MemoryStream(bytes), "text/csv", "Report.csv");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+
+
 
 
 
